e5a24456f845938fb2c45dbc02b9c99d6aa02fd0
jiangrun@hiekn.com <jr_020304>
2017-08-18 15:59:43 +0800

    更新版本号

--- dist/define.less
+++ dist/define.less
@@ -2,7 +2,7 @@
      * @author: 
      *    jiangrun002
      * @version: 
-     *    v0.1.0
+     *    v0.1.2
      * @license:
      *    Copyright 2017, hiknowledge. All rights reserved.
      */
