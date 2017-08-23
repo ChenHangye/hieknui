92c27c84afd48db1e54a0e1e61e9495e67f2223d
jiangrun@hiekn.com <jr_020304>
2017-08-23 17:31:11 +0800

    修复select,dropdown下拉箭头显示异常

--- dist/hieknui.js
+++ dist/hieknui.js
@@ -2,7 +2,7 @@
      * @author: 
      *    jiangrun002
      * @version: 
-     *    v0.1.4
+     *    v0.1.5
      * @license:
      *    Copyright 2017, hiknowledge. All rights reserved.
      */
